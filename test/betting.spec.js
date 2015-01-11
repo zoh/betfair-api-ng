3a266edc462139a85ee2b8905f82ae7c11fd7de4
zoh <zoh@bk.ru>
2015-01-11 11:52:11 +0300

    add betting api work with orders

--- test/betting.spec.js
+++ test/betting.spec.js
@@ -76,4 +76,42 @@ describe('Spec on betting api', function () {
       expect(err).to.be.eql('marketIds has been array with id"s');
     });
   });
+
+  describe('place new order', function () {
+    it('should check market id defined', function () {
+      betting.placeOrders(null, null, null, function (err, res) {
+        expect(err).to.be.eql('marketId has been defined');
+      });
+    });
+
+    it('should check instructions for bets', function () {
+      betting.placeOrders('1.23234234', null, null, function (err, res) {
+        expect(err).to.be.eq('instructions for bets should be defined!');
+      });
+    });
+
+    it('should send request', function (done) {
+      var uu = betting.$unique();
+      var scope = nock('https://api.betfair.com/', {
+        reqheaders: {
+          'X-Application': sessionCfg.applicationKey,
+          'X-Authentication': sessionCfg.token,
+          'Content-Type': 'application/json'
+        }
+      }).post('/exchange/betting/rest/v1.0/placeOrders/', {
+        marketId: '1.23234234',
+        instructions: [{}],
+        customerRef: uu
+      })
+        .reply(200, {});
+
+      betting.placeOrders('1.23234234', [{}], uu, function (err, res) {
+        expect(err).to.not.ok;
+        console.log(arguments);
+        done();
+        scope.done();
+      });
+    });
+
+  });
 });
